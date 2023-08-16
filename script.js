154be17e9a3a98990cd7f80a2e573c1cdb4dde6e
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-08-16 20:26:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
-
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+            var radius = document.getElementById('radius').value;
+            radius = Math.abs(radius);
+            var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+            volume = volume.toFixed(4);
+            document.getElementById('volume').value = volume;
+            return false;
+        }
+ 
+      window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+ 
